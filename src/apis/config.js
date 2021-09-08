9bab2c696e06884654308844ba7caaf879b10bee
Jawad63 <jawadqureshi111@gmail.com>
2021-09-08 16:06:58 +0200

    creating our first component

---
+++ src/apis/config.js
@@ -0,0 +1,2 @@
+export const API_KEY = [fed3814036bd9d640c0d74e6cfe0f004];
+export const API_BASE_URL = 'http://api.openweathermap.org/';
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+// (1): Creating first component: 
+
+const WeatherCard = (props) => {
+   return (
+      <Card style={{ width: '18rem' }}>
+         <Card.Img variant="top" src="holder.js/100px180" />
+         
+         <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+         </Card.Body>
+      </Card>
+   )
+}
+
+export default WeatherCard;
