f8aa4751611f3151c13d8fac185bc04d76ec1237
Jawad63 <jawadqureshi111@gmail.com>
2021-09-08 16:33:04 +0200

    city selector component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import WeatherCard from './components/WeatherCard';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>
-}
+const App = () => {
+  return (
+    <div className="App">
+      {/* dt is in unix-seconds but javascript uses milliseconds, multiply with 1000 */}
+
+      <WeatherCard
+        dt={1602104400 * 1000}
+        temp_min="22.67"
+        temp_max="24.39"
+        main="clear"
+        icon="01d"
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 
 // (1): Creating first component: 
 
-const WeatherCard = (props) => {
+const WeatherCard = ({ dt, temp_min, temp_max, main, icon }) => {
+   const date = new Date(dt);
    return (
       <Card style={{ width: '18rem' }}>
-         <Card.Img variant="top" src="holder.js/100px180" />
+         <Card.Img variant="top"
+            src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
          
          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>{main}Card Title</Card.Title>
+            {/* Date time is recieved in milliseconds, need to turn it into local date time: */}
+            <p>
+               {date.toLocaleDateString()} - {date.toLocaleTimeString()}
+            </p>
+
+            {/* Minimum temprature */}
+            <p>Min: {temp_min}</p>
+
+            {/* Maximum temprature */}
+            <p>Max: {temp_max}</p>
+
          </Card.Body>
       </Card>
    )
