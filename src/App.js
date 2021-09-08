ade8ee66a874e3f1ee75f6993cafd2736d8aaf70
Jawad63 <jawadqureshi111@gmail.com>
2021-09-08 17:01:42 +0200

    working from train

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import WeatherCard from './components/WeatherCard';
 import './App.css';
+import { Conatiner, Container } from 'react-bootstrap';
+import CitySelector from './components/CitySelector';
 
 const App = () => {
   return (
-    <div className="App">
-      {/* dt is in unix-seconds but javascript uses milliseconds, multiply with 1000 */}
-
-      <WeatherCard
-        dt={1602104400 * 1000}
-        temp_min="22.67"
-        temp_max="24.39"
-        main="clear"
-        icon="01d"
-      />
-    </div>
+    <Container className="App">
+      <CitySelector />
+    </Container>
   );
 };
 
--- src/components/CitySelector.js
+++ src/components/CitySelector.js
@@ -1,8 +1,37 @@
 import React, { useState } from 'react';
+import { Row, Col, FormControl, Button } from 'react-bootstrap';
 
 const CitySelector = () => {
    const [city, setCity] = useState('');
-   return <div></div>;
+   return (
+      <>
+         <Row>
+            <Col>
+               <h1>Search your City:</h1>
+            </Col>
+         </Row>
+
+         <Row>
+            <Col xs={4} className="text-center">
+               <FormControl
+                  placeholder="Enter City:"
+               // update your city with user input: 
+                  onChange={(event) => setCity(event.target.value)}
+               // value will be the currently selected city:
+                  value={city}
+               />
+            </Col>
+         </Row>
+
+         <Row>
+            <Col>
+               {/* Event handler for button clicks:  */}
+               <Button onClick= {onSearch}>Check Weather</Button>
+            </Col>
+         </Row>
+      
+      </>
+   );
 };
 
 export default CitySelector;
